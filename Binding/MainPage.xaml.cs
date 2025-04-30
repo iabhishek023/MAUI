6e7b482ba4c8f0d160a8250c1407787b814201c7
Abhishek Puri <abhishek.puri@optimusinfo.com>
2025-04-30 10:58:14 +0530

    Binding done

---
+++ Binding/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using BindingMaui.Models;
+using Microsoft.Maui.Controls;
+
+namespace Binding
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            var person = new Person
+            {
+                Name = "Abhishek",
+                Phone = "56789",
+                Address = "bewhb dhbch"
+
+            };
+
+            Binding personBinding = new Binding();
+
+            personBinding.Source = Person;
+        }
+    }
+
+}
---
+++ Binding/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Binding;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
