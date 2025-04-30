e41639e33e323e2b6d54da65974cd828a2e50322
Abhishek Puri <abhishek.puri@optimusinfo.com>
2025-04-30 11:11:42 +0530

    PerfectPay Done

---
+++ PerfectPayProject/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PerfectPayProject
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PerfectPayProject/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace PerfectPayProject;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
